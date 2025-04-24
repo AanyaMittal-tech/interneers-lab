90fb5b4127eff23b20929aaf7444e78185446adc
Aanya <aanya.mittal.ug22@nsut.ac.in>
2025-04-24 22:39:00 +0530

    Week 6: Added frontend features for displaying products using JS, styled tiles, and API integration

--- backend/django_app/settings.py
+++ backend/django_app/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "django_app",
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -50,7 +51,10 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    'corsheaders.middleware.CorsMiddleware',
 ]
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 ROOT_URLCONF = "django_app.urls"
 
